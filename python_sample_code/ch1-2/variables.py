4dbe7f380e7ae975946fc08466b2063d9aa3b37d
Sarah Zhang <yezhang4528@gmail.com>
2017-10-02 21:42:11 -0400

    Python lesson 1, ch1-2 sample codes

---
+++ python_sample_code/ch1-2/variables.py
@@ -0,0 +1,18 @@
+h = 5
+print("h = ", h)
+
+# invalid declaration
+#3h = 7
+#print("3h = ", 3h)
+
+h3 = 7
+print("h3 = ", h3)
+
+H = 9
+print("h = ", h, ", H = ", H)
+
+fn = 3.14
+print("fn = ", fn)
+
+myStr = "I am a string"
+print("myStr = ", myStr)
